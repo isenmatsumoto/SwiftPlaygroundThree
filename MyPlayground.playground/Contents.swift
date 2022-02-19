ba815ea19f2c3324e2710fe2154e551d2584f60c
isenmatsumoto <isenmats@hawaii.edu>
2022-02-18 17:56:50 -1000

    Completed Parts 1-8

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
 var greeting = "Hello, playground"
+//Part 3
+var fruitNames: [String]
+//var numbers: [Int]
+
+//Part 4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5
+print(fruitNames)
+
+//Part 6
+print("I like to eat\(fruitNames)")
+
+//Part 7: Iterate through all items in the array using the "for-in" syntax
+for name in fruitNames{
+    print("I like to eat" + name)
+}
+
+//Part 8: Introduction to array indexes
+print(fruitNames[1])
